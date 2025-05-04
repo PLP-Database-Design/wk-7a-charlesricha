20c0538cb97c653dccd268004367eea40b3c51e9
Charles Muthui <126247035+charlesricha@users.noreply.github.com>
2025-05-04 11:56:01 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,4 +1,23 @@
--- Question 1: converting to Ist Form
+-- Question 1:
+--Creating Tables
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+
+-- adding data to the tables
+
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- trnsforming this table into 1NF
 
 SELECT OrderID, CustomerName, TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n.n), ',', -1)) AS Product
 FROM ProductDetail
@@ -9,30 +28,34 @@ ORDER BY OrderID, n.n;
 
 
 --Question 2
--- Creating tables so that each non-key column fully depends on the entire primary key
-
--- creating table orders an dinserting data
 
 CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
+    OrderID INT PRIMARY KEY,  -- OrderID is the primary key, uniquely identifying each order
+    CustomerName VARCHAR(100) 
 );
+
+ --Inserting data into the table
+
 INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
+VALUES
+    (101, 'John Doe'),       
+    (102, 'Jane Smith'),     
+    (103, 'Emily Clark');     
 
--- creating table orderdetails an dinserting data
 
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    Product VARCHAR(100),
-    Quantity INT,
-    PRIMARY KEY (OrderID, Product),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+CREATE TABLE Product (
+    OrderID INT,          -- OrderID is a foreign key, referencing the Orders table
+    Product VARCHAR(100),    
+    Quantity INT,         
+    PRIMARY KEY (OrderID, Product), -- Composite primary key: OrderID and Product together
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID) 
 );
-INSERT INTO OrderDetails (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
 
-
-
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+    (101, 'Laptop', 2),    
+    (101, 'Mouse', 1),     
+    (102, 'Tablet', 3),   
+    (102, 'Keyboard', 1), 
+    (102, 'Mouse', 2),     
+    (103, 'Phone', 1);      
